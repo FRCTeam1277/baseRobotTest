94ea0a729844b4cfe576942cc1ff3fb94053659a
chillipenguin <achillipenguin@gmail.com>
2021-12-22 21:00:06 -0500

    fixed average pixel output

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,5 +1,5 @@
-# import cv2
-from cv2 import cv2 #comment this and uncomment top if cv2 member errors
+import cv2
+# from cv2 import cv2 #comment this and uncomment top if cv2 member errors
 import numpy
 import time
 import math
@@ -10,7 +10,9 @@ from networktables import NetworkTables
 NetworkTables.initialize()
 vision_nt = NetworkTables.getTable('Vision')
 
-averagePixel = vision_nt.getEntry("pixel-avg")
+averagePixelR = vision_nt.getEntry("pixel-avg-r")
+averagePixelG = vision_nt.getEntry("pixel-avg-g")
+averagePixelB = vision_nt.getEntry("pixel-avg-b")
 
 def main():
 
@@ -23,9 +25,14 @@ def main():
         if time2 == 0: #if the same frame wait for next
             continue
 
-        input_img.
-        averagePixel.setNumber(cv2.meanStdDev(input_img))
+        meanStdDev = cv2.meanStdDev(input_img)
+        averagePixelR.setNumber(meanStdDev[0][2])
+        averagePixelG.setNumber(meanStdDev[0][1])
+        averagePixelB.setNumber(meanStdDev[0][0])
+
         # if len(pipeline.cascade_classifier_output) >= 1:
             # averagePixel.setNumber(pipeline.cascade_classifier_output[0][3]) #gives width of first face to stream, used to determine if it exists (you can expand on this)
         # else:
             # averagePixel.setNumber(0)
+
+main()
